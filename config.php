374bfebe58ed5e99e36e8b79d08738dee2966742
mrjulio <mrjulio@gmail.com>
2014-02-27 16:04:07 +0200

    db table renamed to avoid name collision

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('DB_HOST',  'localhost');
 define('DB_USER',  'root');
 define('DB_PASS',  'root');
 define('DB_NAME',  'demo');
-define('DB_TABLE', 'revision');
+define('DB_TABLE', 'mrt');
 
 // remember to add a trailing slash
 define('PATH_TO_DELTAS', __DIR__ . '/deltas/');
